118a76175fcc0e31bb906e35f69da4e252edf662
Jack Najarro <jacknajarro1@gmail.com>
2022-05-14 13:26:09 -0500

    NTS

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -15,7 +15,7 @@ const fromApiResponseGifs=apiResponse =>{
     return[]
 }
 
-export default async function getGifs({limit=5, keyword='morty' , page=0 }={}) {
+export default async function getGifs({limit=20, keyword='morty' , page=0 }={}) {
     
     const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=${page * limit}&rating=g&lang=en`
     const res = await fetch(apiURL)
